3ea42586a01dc0982fcddc3dbb38bfda3ef7ac8b
Alonso202 <drosk1711@gmail.com>
2021-06-30 17:37:37 -0400

    se ha agregado base de datos

--- ETEtapa1_AlonsoPerez002D/GroundZero/urls.py
+++ ETEtapa1_AlonsoPerez002D/GroundZero/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index,galeria,login_registro
+
+
 urlpatterns = [
-    path ('', index, name="index"),]
+    path ('', index, name="index"),
+    path('galeria',galeria, name="galeria"),
+    path('login_registro',login_registro, name="login_registro")
+]
