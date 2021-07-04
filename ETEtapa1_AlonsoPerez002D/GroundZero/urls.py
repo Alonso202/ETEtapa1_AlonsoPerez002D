0b6c1dda76f8224a3def859a2ca4000f0bd100b8
Alonso Perez <Alonso202>
2021-07-04 14:49:40 -0400

    Se a agregado eliminar y modificar

--- ETEtapa1_AlonsoPerez002D/GroundZero/urls.py
+++ ETEtapa1_AlonsoPerez002D/GroundZero/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import home,index,form_reg_prov ,listado
+from .views import index,form_reg_prov ,listado,modificar,eliminar
 
 
 
 urlpatterns = [
     path ('', index, name="index"),
-    path('home',home,name="home"),
     path('form_reg_prov',form_reg_prov,name="form_reg_prov"),
-    path('listado',listado,name="listado")
-]
+    path('listado',listado,name="listado"),
+    path('modificar/<NumeroIden>/',modificar,name="modificar"),
+    path('eliminar/<NumeroIden>/',eliminar,name="eliminar"),
+    ]
--- ETEtapa1_AlonsoPerez002D/GroundZero/views.py
+++ ETEtapa1_AlonsoPerez002D/GroundZero/views.py
@@ -1,12 +1,10 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from .models import proveedor
 from .forms import ProvForm
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
-def home(request):
-    return render(request,'home.html' )
 def form_reg_prov(request):
 
     data = {
@@ -30,7 +28,28 @@ def listado(request):
         'proveedores': proveedores
     }
     return render(request,'GroundZero/listado.html',data)
-    
+
+def modificar(request, NumeroIden):
+
+    producto = get_object_or_404(proveedor, NumeroIden=NumeroIden)
+
+    data = {
+        'form': ProvForm(instance=producto)
+    }
+
+    if request.method == 'POST':
+        formulario = ProvForm(data=request.POST, instance=producto, files=request.FILES)
+        if formulario.is_valid():
+            formulario.save()
+            return redirect(to="listado")
+        data["form"] = formulario 
+
+
+    return render(request, 'GroundZero/modificar.html', data)  
+def eliminar(request,NumeroIden):
+    producto = get_object_or_404(proveedor, NumeroIden=NumeroIden)
+    producto.delete()
+    return redirect(to="listado")
 
 
 
